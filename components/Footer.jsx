d3184b6b038d52e1ac498f64b634c0af3bbd4cd3
legodev <gonzalezleonardo283@gmail.com>
2024-01-10 19:09:14 -0300

    Modifying Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,20 @@
+/* eslint-disable jsx-quotes */
+import Link from './Link'
+
 export default function Footer () {
   return (
-    <footer>
-      <section>
-        <article>This is the footer</article>
+    <footer className='footer'>
+      <section className='footer__section'>
+        <ul className='footer__list'>
+          <Link href="#" text="LinkedIn" />
+          <Link href="#" text="Github" />
+          <Link href="#" text="Instagram" />
+        </ul>
+      </section>
+      <section className='footer__section'>
+        <article>
+          <h3 className='footer__copy'>2024 © LEONARDO GONZÁLEZ</h3>
+        </article>
       </section>
     </footer>
   )
