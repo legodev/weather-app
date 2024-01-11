23c5b51c71e402f72c973888ad8311dce272f4dc
legodev <gonzalezleonardo283@gmail.com>
2024-01-11 19:07:59 -0300

    Modifying link component

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,7 +1,13 @@
 export default function Link ({ href, text }) {
   return (
     <li className='footer__li'>
-      <a href={href} className='footer__link'>
+      <a
+        href={href}
+        target='_blank'
+        rel='noopener noreferrer'
+        className='footer__link'
+        aria-label={`Link to ${text}`}
+      >
         {text}
       </a>
     </li>
