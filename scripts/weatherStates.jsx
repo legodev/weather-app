445815f8b5e325836b55543c0afaa92cc033fedd
legodev <gonzalezleonardo283@gmail.com>
2024-02-14 01:17:01 -0300

    Updating state

--- scripts/weatherStates.jsx
+++ scripts/weatherStates.jsx
@@ -4,85 +4,110 @@ import scatteredIcon from '../assets/scattered-clouds.png'
 // Usar un objeto complejo para no tener que importarlo todo de 1 en 1
 
 export const useWeatherStates = () => {
-  const [empty, setEmpty] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const [message, setMessage] = useState('')
-  const [dir, setDir] = useState('/assets/NY.jpg')
-  const [wicon, setWicon] = useState(scatteredIcon)
-  const [iconOne, setIconOne] = useState(scatteredIcon)
-  const [iconTwo, setIconTwo] = useState(scatteredIcon)
-  const [iconThree, setIconThree] = useState(scatteredIcon)
-  const [location, setLocation] = useState('London')
-  const [temp, setTemp] = useState('24°C')
-  const [feels, setFeels] = useState('21°C')
-  const [humidity, setHumidity] = useState('64%')
-  const [min, setMin] = useState('17°C')
-  const [max, setMax] = useState('26°C')
-  const [wind, setWind] = useState('18 km/h')
-  const [pressure, setPressure] = useState('998 hPa')
-  const [weather, setWeather] = useState('Clouds')
-  const [description, setDescription] = useState('Overcast Clouds')
-  const [days, setDays] = useState([
-    '05/12 18:00h',
-    '06/12 18:00h',
-    '07/12 18:00h'
-  ])
-  const [minmax, setMinmax] = useState([
-    '24 / 14 °C',
-    '20 / 10 °C',
-    '22 / 15 °C'
-  ])
-  const [info, setInfo] = useState([
-    'Light Rain',
-    'Overcast Clouds',
-    'Clear Sky'
-  ])
-  const [foreMain, setForeMain] = useState(['Rain', 'Clouds', 'Clear'])
+  const [dataWeather, setDataWeather] = useState({
+    empty: false,
+    loading: false,
+    message: '',
+    dir: '/assets/NY.jpg',
+    wicon: scatteredIcon,
+    iconOne: scatteredIcon,
+    iconTwo: scatteredIcon,
+    iconThree: scatteredIcon,
+    location: 'oslo',
+    temp: '24°C',
+    feels: '21°C',
+    humidity: '64%',
+    min: '17°C',
+    max: '26°C',
+    wind: '18 km/h',
+    pressure: '998 hPa',
+    weather: 'clouds',
+    description: 'overcast clouds',
+    days: ['05/12 18:00h', '06/12 18:00h', '07/12 18:00h'],
+    minmax: ['24 / 14 °C', '20 / 10 °C', '22 / 15 °C'],
+    info: ['light rain', 'overcast clouds', 'clear sky'],
+    foreMain: ['Rain', 'Clouds', 'Clear'],
+  })
+  // const [empty, setEmpty] = useState(false)
+  // const [loading, setLoading] = useState(false)
+  // const [message, setMessage] = useState('')
+  // const [dir, setDir] = useState('/assets/NY.jpg')
+  // const [wicon, setWicon] = useState(scatteredIcon)
+  // const [iconOne, setIconOne] = useState(scatteredIcon)
+  // const [iconTwo, setIconTwo] = useState(scatteredIcon)
+  // const [iconThree, setIconThree] = useState(scatteredIcon)
+  // const [location, setLocation] = useState('London')
+  // const [temp, setTemp] = useState('24°C')
+  // const [feels, setFeels] = useState('21°C')
+  // const [humidity, setHumidity] = useState('64%')
+  // const [min, setMin] = useState('17°C')
+  // const [max, setMax] = useState('26°C')
+  // const [wind, setWind] = useState('18 km/h')
+  // const [pressure, setPressure] = useState('998 hPa')
+  // const [weather, setWeather] = useState('Clouds')
+  // const [description, setDescription] = useState('Overcast Clouds')
+  // const [days, setDays] = useState([
+  //   '05/12 18:00h',
+  //   '06/12 18:00h',
+  //   '07/12 18:00h',
+  // ])
+  // const [minmax, setMinmax] = useState([
+  //   '24 / 14 °C',
+  //   '20 / 10 °C',
+  //   '22 / 15 °C',
+  // ])
+  // const [info, setInfo] = useState([
+  //   'Light Rain',
+  //   'Overcast Clouds',
+  //   'Clear Sky',
+  // ])
+  // const [foreMain, setForeMain] = useState(['Rain', 'Clouds', 'Clear'])
 
   return {
-    empty,
-    setEmpty,
-    loading,
-    setLoading,
-    message,
-    setMessage,
-    dir,
-    setDir,
-    wicon,
-    setWicon,
-    iconOne,
-    setIconOne,
-    iconTwo,
-    setIconTwo,
-    iconThree,
-    setIconThree,
-    location,
-    setLocation,
-    temp,
-    setTemp,
-    feels,
-    setFeels,
-    humidity,
-    setHumidity,
-    min,
-    setMin,
-    max,
-    setMax,
-    wind,
-    setWind,
-    pressure,
-    setPressure,
-    weather,
-    setWeather,
-    description,
-    setDescription,
-    days,
-    setDays,
-    minmax,
-    setMinmax,
-    info,
-    setInfo,
-    foreMain,
-    setForeMain
+    // empty,
+    // setEmpty,
+    // loading,
+    // setLoading,
+    // message,
+    // setMessage,
+    // dir,
+    // setDir,
+    // wicon,
+    // setWicon,
+    // iconOne,
+    // setIconOne,
+    // iconTwo,
+    // setIconTwo,
+    // iconThree,
+    // setIconThree,
+    // location,
+    // setLocation,
+    // temp,
+    // setTemp,
+    // feels,
+    // setFeels,
+    // humidity,
+    // setHumidity,
+    // min,
+    // setMin,
+    // max,
+    // setMax,
+    // wind,
+    // setWind,
+    // pressure,
+    // setPressure,
+    // weather,
+    // setWeather,
+    // description,
+    // setDescription,
+    // days,
+    // setDays,
+    // minmax,
+    // setMinmax,
+    // info,
+    // setInfo,
+    // foreMain,
+    // setForeMain,
+    dataWeather,setDataWeather
   }
 }
